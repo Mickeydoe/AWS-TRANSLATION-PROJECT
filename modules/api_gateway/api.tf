9515ad7b7baeb6abe5f82627b547c92e683c22a1
mickeydoe <michaeldanquah615@gmail.com>
2025-02-09 08:06:15 +0000

    edited tf files

--- modules/api_gateway/api.tf
+++ modules/api_gateway/api.tf
@@ -28,7 +28,8 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   http_method             = aws_api_gateway_method.translate_post.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = var.lambda_function_arn
+  uri                   = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${var.lambda_function_arn}/invocations"
+
 }
 
 resource "aws_api_gateway_deployment" "translation_deployment" {
--- modules/api_gateway/variables.tf
+++ modules/api_gateway/variables.tf
@@ -1,8 +1,8 @@
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS region where the API Gateway and Lambda are deployed"
+  type        = string
   default     = "us-east-1"
 }
-
 variable "lambda_function_arn" {
   description = "ARN of the Lambda function"
 }
